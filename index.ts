f8e27eb2cc118d2f10647c027ef769d994a941d0
Christian Socias <Socias_93@hotmail.com>
2025-11-26 16:52:18 +0100

    created routes and finished api get for categories

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express from "express";
+import categories from "./routes/categories";
 
 const app = express();
 
 const PORT = 5513;
+
+app.use("/api/categories", categories);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
