8d4e3bdef217541753e898befe191f58388f7881
Christian Socias <Socias_93@hotmail.com>
2025-11-26 21:04:07 +0100

    fixed put api for items

--- routes/items.ts
+++ routes/items.ts
@@ -5,11 +5,8 @@ import { validate } from "./schemas/Items";
 const router = express.Router();
 
 const ITEM_API = "/";
-
 const ITEM_API_ID = "/:id";
-
 const ITEM_NOT_FOUND = "Item not found";
-
 const CATEGORY_NOT_FOUND = "Category ID not found";
 
 export interface BaseItem {
@@ -194,6 +191,27 @@ router.post(ITEM_API, (req, res) => {
 
   items.push(newItem);
   return res.status(201).send(newItem);
+});
+
+router.put(ITEM_API_ID, (req, res) => {
+  const item = items.find((i) => i.id === req.params.id);
+  if (!item) return res.status(404).send(ITEM_NOT_FOUND);
+
+  const validation = validate(req.body);
+  if (!validation.success)
+    return res.status(400).send(validation.error.issues[0].message);
+
+  const category = getCategories().find((c) => c.id === req.body.categoryId);
+  if (!category) return res.status(404).send(CATEGORY_NOT_FOUND);
+
+  (item.title = req.body.title), (item.category = category);
+  if (req.body.author !== undefined) (item as any).author = req.body.author;
+  if (req.body.nbrPages !== undefined)
+    (item as any).nbrPages = req.body.nbrPages;
+  if (req.body.runTimeMinutes !== undefined)
+    (item as any).runTimeMinutes = req.body.runTimeMinutes;
+
+  return res.status(200).send(item);
 });
 
 export default router;
