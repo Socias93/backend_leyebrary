fbd6ad106309b34729f79c95f4eeedc9fd2fb0f9
Christian Socias <Socias_93@hotmail.com>
2025-11-30 00:22:05 +0100

    moved PORT from index

--- index.ts
+++ index.ts
@@ -4,10 +4,18 @@ import items from "./routes/items";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
-const PORT = 5313;
+
+app.use(
+  cors({
+    origin: ["https://leyebrary.onrender.com", "http://localhost:5173"],
+  })
+);
+
+app.use(express.json());
 
 app.use("/api/categories", categories);
 app.use("/api/items", items);
+
+const PORT = process.env.PORT || 5313;
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
